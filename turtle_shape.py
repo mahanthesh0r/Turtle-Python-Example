ca0dae81469214af1176463570b996a2a983f1cf
Mahanthesh <monty.officio@gmail.com>
2023-09-10 21:08:16 -0400

    added polygon code

---
+++ turtle_shape.py
@@ -0,0 +1,13 @@
+import turtle as t
+
+sides = int(input('Enter an odd positive integer: '))
+
+if sides % 2 == 0:
+    print('Wrong Input! Please enter a correct value')
+else:
+    angle = 360 / sides
+    for count in range(sides):
+        t.fd(50)
+        t.lt(angle)
+
+t.mainloop()
